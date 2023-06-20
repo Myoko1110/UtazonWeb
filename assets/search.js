654dbffa2b5465d61ac9d93670d73411101b35b4
MyokoJP <myonmyon26@gmail.com>
2023-06-20 22:49:11 +0900

    [Add] 検索バーを使えるように

---
+++ assets/search.js
@@ -0,0 +1,12 @@
+window.onload = function() {
+    const search = document.getElementById("search");
+    const search_query = document.getElementById("search_query");
+    const host = window.location.host;
+    console.log(host)
+
+    search.addEventListener('submit', function (e){
+        e.preventDefault();
+        let query = search_query.value;
+        location.href=`//${host}/search/?q=${query}`;
+    });
+};
