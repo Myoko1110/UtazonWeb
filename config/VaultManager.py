d620b6e249ce04daddd1d6f5df8596d512127169
MyokoJP <myonmyon26@gmail.com>
2023-08-02 22:26:33 +0900

    追加

--- config/VaultManager.py
+++ config/VaultManager.py
@@ -7,13 +7,17 @@ socket_host = settings.SOCKET_HOST
 
 
 def get_player_balance(uuid: str):
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.connect((socket_host, int(socket_port)))
-        s.sendall(uuid.encode())
+    try:
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.connect((socket_host, int(socket_port)))
+            s.sendall(uuid.encode())
 
-        result = s.recv(1024).decode()
+            result = s.recv(1024).decode()
 
-        if result == "Invalid UUID":
-            return False
+            if result == "Invalid UUID":
+                return False
 
-        return float(result)
+            return float(result)
+    except Exception:
+        print("\033[31m" + f"Socketサーバーに接続できませんでした。Socketサーバーの設定はしましたか？")
+        return False
